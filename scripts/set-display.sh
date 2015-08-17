663d48c7947389330c21d76681b61fcbd89c30f7
Micheal Waltz <ecliptik@gmail.com>
2015-08-17 16:54:50 -0700

    Update script to work with external desk monitors and switch display back when just the laptop

--- scripts/set-display.sh
+++ scripts/set-display.sh
@@ -5,9 +5,22 @@
 
 internal="LVDS1"
 
-if xrandr | grep "HDMI1 connected" || xrandr | grep "VGA1 connected" ; then
-    extdisplay=$(xrandr | egrep -e "(HDMI1|VGA1) connected" | awk '{print $1}')
-    xrandr --output ${extdisplay} --mode 1024x768 --output ${internal} --mode 1024x768 --same-as ${extdisplay}
-elif xrandr | grep "HDMI1 disconnected" && xrandr | grep "VGA1 disconnected" ; then
+#Turn off all display
+turnoff() {
+    xrandr --output LVDS1 --off --output HDMI1 --off --output VGA1 --output HDMI2 --off --output VGA2 --off
+}
+
+#For laptop display only
+if xrandr | grep "HDMI1 disconnected" && xrandr | grep "VGA1 disconnected" ; then
+    turnoff
     xrandr --output HDMI1 --off --output VGA1 --off --output ${internal} --auto
+#For two external monitors
+elif xrandr | grep "HDMI2 connected" && xrandr | grep "VGA1 connected" ; then
+    turnoff
+    xrandr --output VIRTUAL1 --auto --output LVDS1 --off --output VGA1 --mode 1680x1050 --pos 3046x0 --rotate left --output HDMI2 --mode 1680x1050 --pos 1366x0 --rotate normal --primary
+#For projectors
+elif xrandr | grep "HDMI1 connected" || xrandr | grep "VGA1 connected" ; then
+    extdisplay=$(xrandr | egrep -e "(HDMI1|VGA1) connected" | awk '{print $1}')
+    turnoff
+    xrandr --output ${extdisplay} --mode 1024x768 --output ${internal} --mode 1024x768 --same-as ${extdisplay}
 fi
